1d4b93044be10879026675763c496cb1fdfe4c28
DevDr <data.devdr@gmail.com>
2022-11-12 20:48:12 +0900

    Second commit

--- src/components/AddMembers.jsx
+++ src/components/AddMembers.jsx
@@ -1,7 +1,10 @@
-
+import { CenteredOverlayForm } from "./CenteredOverlayForm";
 
 export const AddMembers = () => {
-    return(
-        <div>AddMembers component</div>
-    )
-}
+  return (
+    <div>
+      AddMembers component
+      <CenteredOverlayForm />
+    </div>
+  );
+};
--- src/components/CreateGroup.jsx
+++ src/components/CreateGroup.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { groupNameState } from "../state/groupName";
+import { CenteredOverlayForm } from "./CenteredOverlayForm";
 
 export const CreateGroup = () => {
-    return(
-        <div>CreateGroup component</div>
-    )
-}
+  const [validated, setValidated] = useState(false);
+  const [groupName, setGroupName] = useRecoilState(groupNameState);
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    if (form.checkValidity()) {
+        setValidated(true)
+    } else {
+      event.stopPropagation();
+    }
+  };
+  return (
+    <div>
+      <h1>DutchPay</h1>
+      <Container>
+        <Form noValidate onSubmit={handleSubmit}>
+          <Row>
+            <h2>먼저, 더치페이 할 그룹의 이름을 정해볼까요?</h2>
+          </Row>
+          <Row>
+            <Form.Group controlId="vallidationGroupName">
+              <Form.control
+                type="text"
+                required
+                placeholder="2022 제주도 여행"
+              />
+              <Form.control.Feedback type="invalid">
+                그룹 이름을 입력해주세요
+              </Form.control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Button type="submit">저장</Button>
+          </Row>
+        </Form>
+      </Container>
+      {/*<CenteredOverlayForm />*/}
+    </div>
+  );
+};
