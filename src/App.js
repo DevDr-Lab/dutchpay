987adc3200f6f61aa76a289bec4ccfe4a0aa723f
DevDr <data.devdr@gmail.com>
2022-11-12 20:47:55 +0900

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CreateGroup } from './components/CreateGroup';
 import { AddMembers } from './components/AddMembers';
 import { ExpenseMain } from './components/ExpenseMain';
+import { RecoilRoot } from 'recoil';
 
 const App = () => (
     <BrowserRouter>
+    <RecoilRoot>
       <Routes>
-        <Route path="/" element={ <CreateGroup /> } /> /* 전체를 담당하고 있는 컴포넌트를 넘겨줘야 함 */
+        <Route path="/" element={ <CreateGroup /> } /> {/* 전체를 담당하고 있는 컴포넌트를 넘겨줘야 함  */}
         <Route path="/members" element={ <AddMembers /> } />
         <Route path="/expense" element={ <ExpenseMain /> } />
       </Routes>
+    </RecoilRoot>
     </BrowserRouter>
 )
 
